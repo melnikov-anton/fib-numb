bc762bf8e54db441c63cff93595beabe605fc791
Anton Melnikov <antonmelnikov@Antons-MacBook-Pro.local>
2020-02-25 17:03:36 +0300

    Small refactoring

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
 var express = require('express');
+var path = require('path');
 var app = express();
 
-
+app.use('/static', express.static(__dirname + '/frontend/build/static'));
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.set('Content-Type', 'text/html');
+  res.sendFile(path.join(__dirname + '/frontend/build/index.html'));
 });
 
 app.get('/api', function (req, res) {
 
-  res.send({id: 1, title:'Post 1'});
+  res.json({id: 1, title:'Post 1'});
 });
 
 app.listen(3000, function () {
