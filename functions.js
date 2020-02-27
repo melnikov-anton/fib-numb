2e88caf6824af0686dd75a39a521fc7ecaead4c7
Anton Melnikov <antonmelnikov@Antons-MacBook-Pro.local>
2020-02-27 22:55:05 +0300

    General function

---
+++ functions.js
@@ -0,0 +1,31 @@
+
+function getFibMember(member) {
+  if (member == 0) {
+    return 0;
+  }
+  let fibMemb = [];
+  for (let i = 0; i < member; i++) {
+    if (i === 0) {
+      fibMemb[i] = 0;
+    } else if (i === 1) {
+      fibMemb[i] = 1;
+    } else {
+      fibMemb[i] = fibMemb[i-2] + fibMemb[i-1];
+    }
+  }
+  return fibMemb[member-1] === null ? 0 : fibMemb[member-1];
+}
+
+function getClientIP(rawIP) {
+  let ip = rawIP;
+  ip = ip.toString();
+  ip = ip.split(':');
+  ip = ip[ip.length - 1];
+  ip = ip === '1' ? '127.0.0.1' : ip;
+  return ip;
+}
+
+module.exports = {
+  getFibMember,
+  getClientIP
+}
